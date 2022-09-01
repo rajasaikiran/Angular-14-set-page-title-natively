bfc9b4f932155dcf8172e038af0a58f9fee87576
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-09-01 10:34:41 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+//Using Resolvers to set page titles
+@Injectable({ providedIn: 'root' })
+export class CustomTitleResolver {
+  resolve() {
+    return Promise.resolve('Random Title');
+  }
+}
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'app-home',
+  },
+  { 
+    path: 'about', 
+    component: AboutComponent,
+    title: CustomTitleResolver
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
